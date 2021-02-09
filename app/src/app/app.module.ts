5bb611249f962aa6616a441c2ab88af931107275
Leo Kondratev <39639000+Mariownyou@users.noreply.github.com>
2021-02-09 18:55:29 +0300

    добавил dragNdrop

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropComponent } from './components/drag-drop/drag-drop.component';
+import { HomeComponent } from './pages/home/home.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragDropComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
